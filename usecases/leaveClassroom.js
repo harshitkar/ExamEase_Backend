c14579d39654cf5196f43c0fb24d363bb6ae4e35
harsh <harshitkar104@gmail.com>
2025-02-28 21:27:20 +0530

    auth and classroom complete

--- usecases/leaveClassroom.js
+++ usecases/leaveClassroom.js
@@ -4,17 +4,18 @@ class LeaveClassroom {
     }
 
     async execute({ user_id, classroom_id }) {
-        const isLastTeacher = await this.classroomRepository.countTeachersInClassroom(classroom_id) === 1;
-
+        const teacherCount = await this.classroomRepository.countTeachersInClassroom(classroom_id);
+        const isLastTeacher = teacherCount === 1;
+    
         if (isLastTeacher) {
-        await this.classroomRepository.deleteClassroom(classroom_id);
-        await this.classroomRepository.deleteUserAccount(user_id);
-        return { message: "You were the last teacher. Classroom and your account have been deleted." };
+            await this.classroomRepository.deleteClassroom(classroom_id);
+            await this.classroomRepository.deleteUserAccount(user_id);
+            return { message: "You were the last teacher. Classroom and your account have been deleted." };
         } else {
-        await this.classroomRepository.removeUserFromClassroom(user_id, classroom_id);
-        return { message: "You have left the classroom." };
+            await this.classroomRepository.removeUserFromClassroom(user_id, classroom_id);
+            return { message: "You have left the classroom." };
         }
-    }
+    }    
 }
 
 module.exports = LeaveClassroom;  
